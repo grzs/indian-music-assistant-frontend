ba903a676493a4019eb4c792668f639e679b284e
Janos Gerzson <janos.gerzson@vertis.com>
2025-11-10 13:08:30 +0100

    [IMP] CSS, dividing code to separate files

---
+++ src/Composition.tsx
@@ -0,0 +1,144 @@
+import { useState, useEffect } from 'react'
+
+function MatraItem({ value, itemType, matraNr, subsectionNr, sectionName, sectionNr, matraCounter, onMatraInputChange }) {
+  if (itemType == "number") {
+    return (
+      <div className={itemType} data-matra-nr-global={matraCounter}>{value}</div>
+    )
+  }
+  return (
+    <div
+      className={itemType}
+      data-matra-nr-global={matraCounter}
+    >
+      <div className="matraitem" data-matra-nr-global={matraCounter}>
+        {value}
+      </div>
+      <input
+        name={`${sectionName}-${subsectionNr}-${matraNr}-${itemType}`}
+        value={value}
+        data-type={itemType}
+        data-matra-nr={matraNr}
+        data-subsection-nr={subsectionNr}
+        data-section-name={sectionName}
+        data-section-nr={sectionNr}
+        onChange={onMatraInputChange}
+      />
+    </div>
+  )
+}
+
+function Matra({
+  matra,
+  matraNr,
+  subsectionNr,
+  subsectionType,
+  sectionName,
+  sectionNr,
+  matraCounter,
+  position,
+  onMatraInputChange,
+  onMatraSelect
+}) {
+  matra["number"] = matraCounter + 1
+
+  let classList = ["matra"]
+  if (matraNr == 0) classList.push(subsectionType)
+  if (position == matraCounter) classList.push("active")
+
+  return (
+    <div
+      data-matra-nr-global={matraCounter}
+      onClick={onMatraSelect}
+      className={classList.join(" ")}
+    >
+      {Object.entries(matra).map(([key, value]) => (
+        <MatraItem
+          key={key}
+          value={value}
+          itemType={key}
+          matraNrGlobal={matraCounter}
+          matraNr={matraNr}
+          subsectionNr={subsectionNr}
+          sectionName={sectionName}
+          sectionNr={sectionNr}
+          matraCounter={matraCounter}
+          onMatraInputChange={onMatraInputChange}
+        />
+      ))}
+    </div>
+  )
+}
+
+function SubSection({ subsection, subsectionNr, sectionName, sectionNr, matraCounter, position, onMatraInputChange, onMatraSelect }) {
+  return (
+    <div className={position in matraCounter ? `subsection ${subsection.type} active` : `subsection ${subsection.type}`}>
+      {subsection.matras.map((matra, i) => (
+        <Matra
+          key={i}
+          matra={matra}
+          matraNr={i}
+          subsectionNr={subsectionNr}
+          subsectionType={subsection.type}
+          sectionName={sectionName}
+          sectionNr={sectionNr}
+          matraCounter={matraCounter[i]}
+          position={position}
+          onMatraInputChange={onMatraInputChange}
+          onMatraSelect={onMatraSelect}
+        />
+      ))}
+    </div>
+  )
+}
+
+function Section({ section, sectionNr, matraCounter, position, onMatraInputChange, onMatraSelect }) {
+  return (
+    <div className="row">
+      <h2>{section.name}</h2>
+      <div className="section">
+        {section.subsections.map((subsection, i) => (
+          <SubSection
+            key={i}
+            subsection={subsection}
+            subsectionNr={i}
+            sectionName={section.name}
+            sectionNr={sectionNr}
+            matraCounter={matraCounter[i]}
+            position={position}
+            onMatraInputChange={onMatraInputChange}
+            onMatraSelect={onMatraSelect}
+          />
+        ))}
+      </div>
+    </div>
+  )  
+}
+
+function Composition({ sections, cursorPosition, matraCounter, onMatraInputChange, onMatraSelect }) {
+  const [position, setPosition] = useState(0)
+
+  useEffect(() => {
+    const compLength = matraCounter.flat(2).length
+    const nextPosition = cursorPosition % compLength
+    setPosition(nextPosition)
+  }, [sections, cursorPosition])
+
+  return (
+    <div className="composition">
+      {sections.map((section, i) => (
+        <Section
+          key={section.name}
+          section={section}
+          sectionNr={i}
+          matraCounter={matraCounter[i]}
+          position={position}
+          onMatraInputChange={onMatraInputChange}
+          onMatraSelect={onMatraSelect}
+        />
+      ))}
+    </div>
+  )
+}
+
+export { Composition }
---
+++ src/testdata.js
@@ -0,0 +1,225 @@
+export default {
+  "taals": {
+    "chau": {
+      "slug": "chau",
+      "name": "Chau Taal",
+      "type": "taal",
+      "sections": [
+        {
+          "name": "theka",
+          "subsections": [
+            {
+              "type": "sam",
+              "matras": [
+                {
+                  "syllable": "Dha"
+                },
+                {
+                  "syllable": "Dha"
+                }
+              ]
+            },
+            {
+              "type": "khali",
+              "matras": [
+                {
+                  "syllable": "Dhin"
+                },
+                {
+                  "syllable": "Ta"
+                }
+              ]
+            },
+            {
+              "type": "tali",
+              "matras": [
+                {
+                  "syllable": "Kite"
+                },
+                {
+                  "syllable": "Dha"
+                }
+              ]
+            },
+            {
+              "type": "khali",
+              "matras": [
+                {
+                  "syllable": "Dhin"
+                },
+                {
+                  "syllable": "Ta"
+                }
+              ]
+            },
+            {
+              "type": "tali",
+              "matras": [
+                {
+                  "syllable": "Tite"
+                },
+                {
+                  "syllable": "Kate"
+                }
+              ]
+            },
+            {
+              "type": "tali",
+              "matras": [
+                {
+                  "syllable": "Ghade"
+                },
+                {
+                  "syllable": "Ghana"
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  },
+  "bandish": {
+    "slug": "anandi-jagabandi",
+    "name": "Anandi Jagabandi",
+    "type": "bandish",
+    "taal": "chau",
+    "sections": [
+      {
+        "name": "sthayi",
+        "subsections": [
+          {
+            "type": "line",
+            "matras": [
+              {
+                "syllable": "A",
+                "sargam": "P",
+                "bol": "/"
+              },
+              {
+                "syllable": "-",
+                "sargam": "m",
+                "bol": "-"
+              },
+              {
+                "syllable": "nan",
+                "sargam": "G",
+                "bol": "-"
+              },
+              {
+                "syllable": "-",
+                "sargam": "-",
+                "bol": "c"
+              },
+              {
+                "syllable": "di",
+                "sargam": "G",
+                "bol": "\\"
+              },
+              {
+                "syllable": "-",
+                "sargam": "R",
+                "bol": "-"
+              },
+              {
+                "syllable": "Ja",
+                "sargam": "S",
+                "bol": "/"
+              },
+              {
+                "syllable": "ga",
+                "sargam": "N",
+                "bol": "/"
+              },
+              {
+                "syllable": "-",
+                "sargam": "D",
+                "bol": "-"
+              },
+              {
+                "syllable": "ban",
+                "sargam": "N",
+                "bol": "/"
+              },
+              {
+                "syllable": "-",
+                "sargam": "R",
+                "bol": "-"
+              },
+              {
+                "syllable": "di",
+                "sargam": "s",
+                "bol": "\\"
+              }
+            ]
+          },
+          {
+            "type": "line",
+            "matras": [
+              {
+                "syllable": "A",
+                "sargam": "P",
+                "bol": "/"
+              },
+              {
+                "syllable": "-",
+                "sargam": "m",
+                "bol": "-"
+              },
+              {
+                "syllable": "nan",
+                "sargam": "G",
+                "bol": "-"
+              },
+              {
+                "syllable": "-",
+                "sargam": "-",
+                "bol": "c"
+              },
+              {
+                "syllable": "di",
+                "sargam": "G",
+                "bol": "\\"
+              },
+              {
+                "syllable": "-",
+                "sargam": "R",
+                "bol": "-"
+              },
+              {
+                "syllable": "Ja",
+                "sargam": "S",
+                "bol": "/"
+              },
+              {
+                "syllable": "ga",
+                "sargam": "N",
+                "bol": "/"
+              },
+              {
+                "syllable": "-",
+                "sargam": "D",
+                "bol": "-"
+              },
+              {
+                "syllable": "ban",
+                "sargam": "N",
+                "bol": "/"
+              },
+              {
+                "syllable": "-",
+                "sargam": "R",
+                "bol": "-"
+              },
+              {
+                "syllable": "di",
+                "sargam": "s",
+                "bol": "\\"
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
+}
